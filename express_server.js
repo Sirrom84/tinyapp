c80450bd8045426588d53618232a5c1c64f2c3fa
Rob <morrisrjc@gmail.com>
2021-01-19 04:26:10 +0000

    added key generator function and installed body-parser

--- express_server.js
+++ express_server.js
@@ -10,6 +10,14 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get('/urls', (req,res) => {
   const templateVars = {
     urls: urlDatabase
@@ -17,10 +25,13 @@ app.get('/urls', (req,res) => {
   res.render("urls_index", templateVars)
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
-app.get("/urls/:shortURL", (req,res) => { //do not understand this part at all
+app.get("/urls/:id", (req,res) => { //do not understand this part at all
   const templateVars = {
-    shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] 
+    shortURL: req.params.id, longURL: urlDatabase[req.params.id] 
   }
 });
 
@@ -39,3 +50,9 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Tiny app listening on port ${PORT}!`);
 });
+
+const generateRandomString = () => {
+
+  return Math.random().toString(36).slice(7);
+
+};
